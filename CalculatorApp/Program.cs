e1571cf64cc14a2353a1c4055fcee503694dd23e
BlaiseAddeh <addehblaise@yahoo.fr>
2021-08-27 14:00:12 +0200

    Creation projet- Creation et appel de la méthode Addition retournant entier

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("-----------Creation de la fonction Addition");
+            int result = Addition(3, 5);
+            Console.WriteLine($"L'addition de 3 et 5 égale à : {result}");
+            Console.ReadKey();
+        }
+
+        private static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
