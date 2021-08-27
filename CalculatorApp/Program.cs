ae5746d107cfd1086784e10761827fd43af573c7
ADDEH BLAISE <b.addeh@my-kiwi.fr>
2021-08-27 17:21:04 +0200

    Creation et appel de la méthode Addition avec des décimaux comme paramètres et retournant un decimal

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -10,6 +10,8 @@ namespace CalculatorApp
             Console.WriteLine("-----------Creation de la fonction Addition");
             int result = Addition(3, 5);
             Console.WriteLine($"L'addition de 3 et 5 égale à : {result}");
+            decimal result2 = Addition(2.3m, 6.7m);
+            Console.WriteLine($"L'addition de 2.3 et 6.7 égale à : {result2}");
             Console.ReadKey();
         }
 
@@ -17,5 +19,11 @@ namespace CalculatorApp
         {
             return a + b;
         }
+
+        private static decimal Addition(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
     }
 }
